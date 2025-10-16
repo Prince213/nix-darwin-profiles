8960e589f03890742d6b12b93b78ce6195272bff
Sizhe Zhao <prc.zhao@outlook.com>
2025-10-16 13:17:56 +0800

    flake: add formatter

--- flake.nix
+++ flake.nix
@@ -2,14 +2,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ flake-parts, treefmt-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
       systems = [
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+      perSystem = {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+        };
+      };
     };
 }
