38a929d1a3719b80e45c4d0d8b5de69d209010f7
Sizhe Zhao <prc.zhao@outlook.com>
2025-10-18 14:24:44 +0800

    nix-darwin: refactor & support installing from path

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -7,31 +7,75 @@
 let
   myLib = import ./lib.nix { inherit lib pkgs; };
   format = myLib.plistFormat;
+
+  cfg = config.system.profiles;
+  removedProfileIdentifiers = lib.mapAttrsToList (n: _: n) (
+    lib.filterAttrs (_: v: v.enable == false) cfg.configuration
+  );
+  installedProfiles = lib.mapAttrs (_: v: v.path) (
+    lib.filterAttrs (_: v: v.enable == true) cfg.configuration
+  );
 in
 {
   options.system.profiles = {
-    knownProfiles = lib.mkOption {
-      description = ''
-        List of system profile identifiers owned and managed by nix-darwin.
-        Used to indicate what profiles are safe to install/remove based on the configuration.
-      '';
-      default = [ ];
-      type = lib.types.listOf lib.types.str;
-    };
-    profiles = lib.mkOption {
+    configuration = lib.mkOption {
       description = ''
         System configuration profiles.
       '';
       default = { };
       type = lib.types.attrsOf (
-        lib.types.either lib.types.path (
-          lib.types.submodule {
-            freeformType = format.type;
+        lib.types.submodule (
+          { name, config, ... }:
+          {
+            options = {
+              enable = lib.mkOption {
+                type = lib.types.nullOr lib.types.bool;
+                default = null;
+                description = ''
+                  Whether to enable this profile.
+                '';
+              };
+              path = lib.mkOption {
+                type = lib.types.path;
+                description = ''
+                  Path to the profile file.
+                '';
+              };
+              topLevel = lib.mkOption {
+                type = lib.types.submodule {
+                  freeformType = format.type;
+                  config = {
+                    PayloadIdentifier = name;
+                    PayloadScope = "System";
+                    PayloadType = "Configuration";
+                    PayloadVersion = 1;
+                    TargetDeviceType = 5;
+                  };
+                };
+                default = { };
+              };
+              payloads = lib.mkOption {
+                type = lib.types.listOf (
+                  lib.types.submodule {
+                    freeformType = format.type;
+                    config = {
+                      PayloadVersion = 1;
+                    };
+                  }
+                );
+                default = [ ];
+              };
+            };
             config = {
-              PayloadType = "Configuration";
-              PayloadScope = "System";
-              PayloadVersion = 1;
-              TargetDeviceType = 5;
+              path = lib.mkIf (config.payloads != [ ]) (
+                lib.mkDefault (
+                  format.generate "${name}.mobileconfig" (
+                    lib.recursiveUpdate config.topLevel {
+                      PayloadContent = config.payloads;
+                    }
+                  )
+                )
+              );
             };
           }
         )
@@ -39,39 +83,25 @@ in
     };
   };
 
-  config =
-    let
-      cfg = config.system.profiles;
-      allProfileIdentifiers = lib.attrNames cfg.profiles;
-      removedProfileIdentifiers = lib.filter (n: !lib.elem n allProfileIdentifiers) cfg.knownProfiles;
-      installedProfileIdentifiers = lib.filter (n: lib.elem n allProfileIdentifiers) cfg.knownProfiles;
-      installedProfiles = lib.mapAttrs (
-        n: v:
-        if lib.isPath v then
-          v
-        else
-          (format.generate "${n}.mobileconfig" (lib.recursiveUpdate v { PayloadIdentifier = n; }))
-      ) (lib.filterAttrs (n: _: lib.elem n installedProfileIdentifiers) cfg.profiles);
-    in
-    {
-      system.activationScripts.extraActivation.text = ''
-        echo "updating system configuration profiles..." >&2
+  config = {
+    system.activationScripts.extraActivation.text = ''
+      echo "updating system configuration profiles..." >&2
 
-        ${lib.concatMapStringsSep "\n" (profile: ''
-          if test -n "${myLib.installDateForProfileOfUser "_computerlevel" profile}"; then
-            echo "removing ${profile}"
-            /usr/bin/profiles remove -type configuration -identifier "${profile}" -forced
-          fi
-        '') removedProfileIdentifiers}
+      ${lib.concatMapStringsSep "\n" (profile: ''
+        if test -n "${myLib.installDateForProfileOfUser "_computerlevel" profile}"; then
+          echo "removing ${profile}"
+          /usr/bin/profiles remove -type configuration -identifier "${profile}" -forced
+        fi
+      '') removedProfileIdentifiers}
 
-        ${lib.concatMapAttrsStringSep "\n" (n: v: ''
-          echo "installing ${n}"
-          oldProfileInstallDate="${myLib.installDateForProfileOfUser "_computerlevel" n}"
-          /usr/bin/open x-apple.systempreferences:com.apple.Profiles-Settings.extension ${v}
-          while test "$oldProfileInstallDate" == "${myLib.installDateForProfileOfUser "_computerlevel" n}"; do
-            sleep 1
-          done
-        '') installedProfiles}
-      '';
-    };
+      ${lib.concatMapAttrsStringSep "\n" (n: v: ''
+        echo "installing ${n}"
+        oldProfileInstallDate="${myLib.installDateForProfileOfUser "_computerlevel" n}"
+        /usr/bin/open x-apple.systempreferences:com.apple.Profiles-Settings.extension ${v}
+        while test "$oldProfileInstallDate" == "${myLib.installDateForProfileOfUser "_computerlevel" n}"; do
+          sleep 1
+        done
+      '') installedProfiles}
+    '';
+  };
 }
